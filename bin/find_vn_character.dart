9858cdbb4cfeba6da07229a1cba4dba9be4451c3
sinhle <sinhle.dev@gmail.com>
2024-02-16 19:25:17 +0700

    init

---
+++ bin/find_vn_character.dart
@@ -0,0 +1,5 @@
+import 'package:find_vn_character/find_vn_character.dart' as find_vn_character;
+
+void main(List<String> arguments) {
+  print('Hello world: ${find_vn_character.calculate()}!');
+}
---
+++ test/find_vn_character_test.dart
@@ -0,0 +1,8 @@
+import 'package:find_vn_character/find_vn_character.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
